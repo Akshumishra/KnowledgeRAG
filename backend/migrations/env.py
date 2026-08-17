aad1b064bd7a77fba2c3e0a53148432f5df64114
Akshumishra <akshitamishra421@gmail.com>
2026-08-17 16:11:15 +0530

    fix: escape % in database_url for alembic configparser

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -19,7 +19,7 @@ from src.models.settings import *
 
 config = context.config
 
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", settings.database_url.replace("%", "%%"))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
